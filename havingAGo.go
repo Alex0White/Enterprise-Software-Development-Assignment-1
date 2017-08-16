28739c6566205fffc0d09d13fff5f13900a11e37
Alex0White <alexjwhitex@gmail.com>
2017-08-16 14:45:29 +1200

    got the structs to nest
    i have layed the eggs

--- havingAGo.go
+++ havingAGo.go
@@ -4,6 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"strconv"
+	"time"
+
+	"github.com/pkg/profile"
 )
 
 func check(e error) {
@@ -18,58 +22,47 @@ database{
 	students: [...]
 }
 */
-
-type marks struct {
-	class      string  `json:"class"`
-	student_id int     `json:"student_id"`
-	marks      float32 `json:"mark"`
-}
-
-type students struct {
-	student_id   int    `json:"student_id"`
-	first_name   string `json:"first_name"`
-	last_name    string `json:"last_name"`
-	age          int    `json:"age"`
-	phone_number string `json:"phone_numebr"`
-	suburb       string `json:"suburb"`
-	city         string `json:"city"`
-}
-type database struct {
-	theMarks    []marks    `json:"marks"`
-	theStudents []students `json:"students"`
+type Database struct {
+	Marks []struct {
+		StudentID int     `json:"student_id"`
+		Class     string  `json:"class"`
+		Mark      float64 `json:"mark"`
+	} `json:"marks"`
+	Students []struct {
+		StudentID   int    `json:"student_id"`
+		FirstName   string `json:"first_name"`
+		LastName    string `json:"last_name"`
+		Age         int    `json:"age"`
+		PhoneNumebr string `json:"phone_numebr"`
+		Suburb      string `json:"suburb"`
+		City        string `json:"city"`
+	} `json:"students"`
 }
 
 func main() {
-
+	defer profile.Start(profile.MemProfile, profile.CPUProfile, profile.ProfilePath(".")).Stop()
 	//var dat map[string]interface{}
-	var dat map[string]interface{}
+	time.Sleep(1 * time.Second)
 
+	var d Database
 	jsonFile, err := ioutil.ReadFile("data.json")
-	//fmt.Println(jsonFile)
 	check(err)
-	//fmt.Print(string(jsonFile))
-
-	//var dat map[string]interface{}
-
-	/*if err := json.Unmarshal(jsonFile, &d); err != nil {
-		panic(err)
-	}*/
-	if err := json.Unmarshal(jsonFile, &dat); err != nil {
+	if err := json.Unmarshal(jsonFile, &d); err != nil {
 		panic(err)
 	}
-	marksArray := dat["marks"]
-	//database := dat["marks"], dat["students"]
-	//database{dat["marks"], dat["students"]}
-	//fmt.Println(dat)
-	fmt.Println(marksArray)
 
-	/*
-		res1D := &Response1{
-			Page:   1,
-			Fruits: []string{"apple", "peach", "pear"}}
+	for i := 0; i < len(d.Students); i++ {
+		var studentMarks string = ""
+		var studentNames string = d.Students[i].FirstName + " " + d.Students[i].LastName + " "
+		for chur := 0; chur < len(d.Marks); chur++ {
 
-		res1B, _ := json.Marshal(res1D)
-		fmt.Println(string(res1B))
-	*/
+			if i == d.Marks[chur].StudentID {
+				FlStr := strconv.FormatFloat(d.Marks[chur].Mark, 'f', 2, 64)
+				studentMarks += d.Marks[chur].Class + ": " + FlStr + " "
+			}
+		}
+		fmt.Println(studentNames + studentMarks)
+	}
+	//fmt.Println(d.Marks[0].Class)
 
 }
